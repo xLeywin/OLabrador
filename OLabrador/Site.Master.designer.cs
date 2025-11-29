9b0c10a9075d043651f1744a42053553fce22ece
Walker Viani <walkeryslan@gmail.com>
2025-11-29 16:41:53 -0300

    mudanças no cabeçalho

--- OLabrador/Site.Master.designer.cs
+++ OLabrador/Site.Master.designer.cs
@@ -33,15 +33,6 @@ namespace OLabrador
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// Controle Logo.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image Logo;
-
-        /// <summary>
         /// Controle Image1.
         /// </summary>
         /// <remarks>
